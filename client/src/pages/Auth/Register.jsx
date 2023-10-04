9235179733c275a287140a7b0f951de22a591f2c
Aditya <192105adityashah@gmail.com>
2023-10-04 11:35:47 +0530

    Added login page

---
+++ client/src/pages/Auth/Register.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+        <div className="container my-12 mx-auto px-4 md:px-12">
+  <div className="flex flex-wrap -mx-1 lg:-mx-4">
+    <div className="my-1 px-1 max:w-1/2 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 "> 
+      <article className="overflow-hidden rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-75 duration-300 image-container shadow-lg shadow-black">
+        <a href="#">
+          <img alt="Placeholder" className="block h-auto w-full" src="1.png" />
+        </a>
+      </article>
+    </div>
+    <div className="my-1 px-1 max:w-1/2 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 ">
+      <article className="overflow-hidden rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-75 duration-300 image-container shadow-lg shadow-black">
+        <a href="#">
+          <img alt="Placeholder" className="block h-auto w-full" src="2.png" />
+        </a>
+      </article>
+    </div>
+    <div className="my-1 px-1 max:w-1/2 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 "> 
+      <article className="overflow-hidden rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-75 duration-300 image-container shadow-lg shadow-black">
+        <a href="#">
+          <img alt="Placeholder" className="block h-auto w-full" src="3.png" />
+        </a>
+      </article>
+    </div>
+    <div className="my-1 px-1 max:w-1/2 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 ">
+      <article className="overflow-hidden rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-75 duration-300 image-container shadow-lg shadow-black">
+        <a href="#">
+          <img alt="Placeholder" className="block h-auto w-full" src="4.png" />
+        </a>
+      </article>
+    </div>
+    <div className="my-1 px-1 max:w-1/2 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 "> 
+      <article className="overflow-hidden rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-75 duration-300 image-container shadow-lg shadow-black">
+        <a href="#">
+          <img alt="Placeholder" className="block h-auto w-full" src="5.png" />
+        </a>
+      </article>
+    </div>
+    <div className="my-1 px-1 max:w-1/2 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 "> 
+      <article className="overflow-hidden rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-75 duration-300 image-container shadow-lg shadow-black">
+        <a href="#">
+          <img alt="Placeholder" className="block h-auto w-full" src="6.png" />
+        </a>
+      </article>
+    </div>
+    <div className="my-1 px-1 max:w-1/2 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 "> 
+      <article className="overflow-hidden rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-75 duration-300 image-container shadow-lg shadow-black">
+        <a href="#">
+          <img alt="Placeholder" className="block h-auto w-full" src="1.png" />
+        </a>
+      </article>
+    </div>
+    <div className="my-1 px-1 max:w-1/2 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 "> {/* Adjusted width for lg */}
+      <article className="overflow-hidden rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-75 duration-300 image-container shadow-lg shadow-black">
+        <a href="#">
+          <img alt="Placeholder" className="block h-auto w-full" src="2.png" />
+        </a>
+      </article>
+    </div>
+    <div className="my-1 px-1 max:w-1/2 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 "> {/* Adjusted width for lg */}
+      <article className="overflow-hidden rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-75 duration-300 image-container shadow-lg shadow-black">
+        <a href="#">
+          <img alt="Placeholder" className="block h-auto w-full" src="3.png" />
+        </a>
+      </article>
+    </div>
+  </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Register
