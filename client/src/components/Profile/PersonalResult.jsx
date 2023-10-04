2326c2329426130443a5e0423cea632fc84ec517
Aditya <192105adityashah@gmail.com>
2023-10-05 02:00:12 +0530

    new UI

--- client/src/components/Profile/PersonalResult.jsx
+++ client/src/components/Profile/PersonalResult.jsx
@@ -7,6 +7,11 @@ const PersonalResult = () => {
     { subject: "History", score: 95 },
     { subject: "English", score: 80 },
   ];
+  const skills = [
+    { subject: "PERSONALITY ASSESSMENT", progress: 80 },
+    { subject: "INTEREST ASSESSMENT", progress: 60 },
+    { subject: "APTITUDE TEST", progress: 90 }
+  ];
   return (
     <div>
       <div className="max-w-md mx-auto p-4 bg-blue-100 rounded-lg shadow-lg">
@@ -20,14 +25,23 @@ const PersonalResult = () => {
       </div>
 
       <div className="max-w-md mx-auto bg-white rounded p-6 mt-6 flex items-center">
-        <h2 className="text-xl font-bold mb-4 mr-4">Download Assessment Result</h2>
-        <button
-          id="downloadBtn"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
-        >
-          <i className="fas fa-file-download"></i> 
-        </button>
+        <h2 className="text-xl font-bold mb-4 mr-4">Download Assessment Result &nbsp;<i className="fa-solid fa-file-arrow-down text-blue-700 fa-xl"></i></h2>
       </div>
+
+      {skills.map((skill, index) => (
+        <div key={index} className="max-w-md bg-white shadow-lg rounded-lg overflow-hidden mx-auto my-4">
+          <div className="px-4 py-2">
+            <h2 className="text-lg font-semibold text-gray-800 mb-2">{skill.subject}</h2>
+            <div className="flex items-center">
+              <div className="text-sm text-gray-600 mr-2">Progress</div>
+              <div className="text-sm text-blue-500 font-semibold">{skill.progress}%</div>
+            </div>
+            <div className="w-full bg-gray-300 mt-2 h-2 rounded-full">
+              <div style={{ width: `${skill.progress}%` }} className="bg-blue-500 h-full rounded-full"></div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
--- client/src/pages/Jobs.jsx
+++ client/src/pages/Jobs.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import MainLayout from "../components/Layout/ProfileLayout";
 import axios from "axios";
 import Loader from "../components/Loader";
+import IMG from "../images/abc.png";
 
 const Jobs = () => {
   const [jobs, setJobs] = useState([]);
@@ -31,7 +32,7 @@ const Jobs = () => {
   };
 
   useEffect(() => {
-    fetchJobs();
+    // fetchJobs();
   }, []);
 
   useEffect(() => {
@@ -48,33 +49,38 @@ const Jobs = () => {
 
   return (
     <MainLayout>
-      <h1 className="text-center mt-10 text-4xl">Suggested jobs</h1>
-      <div className="container mx-auto px-4 py-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
-  {Array.isArray(jobs) &&
-    jobs?.map((job, index) => (
-      <div
-        className="w-full mx-auto bg-white rounded-md shadow-lg p-6 space-y-4"
-        key={index}
-      >
-        {job.employer_logo ? (
-          <img src={job.employer_logo} alt="" className="h-16 max-w-full" />
-        ) : (
-          <div className="h-16"></div> // Placeholder for image if not present
-        )}
-
-        <h1 className="text-xl font-bold">{job.job_title}</h1>
-
-        <p className="text-gray-700">{job.employer_company_type}</p>
-        <p className="text-gray-700">{job.employer_name}</p>
-
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Apply Now
-        </button>
+      <div class="flex items-center">
+        <img src={IMG} alt="" class="mr-4 w-40" />
+        <h1 class="text-center text-4xl">Suggested jobs</h1>
       </div>
-    ))}
-</div>
+      <div className="container mx-auto px-4 py-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+        {Array.isArray(jobs) &&
+          jobs?.map((job, index) => (
+            <div
+              className="w-full mx-auto bg-white rounded-md shadow-lg p-6 space-y-4"
+              key={index}
+            >
+              {job.employer_logo ? (
+                <img
+                  src={job.employer_logo}
+                  alt=""
+                  className="h-16 max-w-full"
+                />
+              ) : (
+                <div className="h-16"></div> // Placeholder for image if not present
+              )}
 
+              <h1 className="text-xl font-bold">{job.job_title}</h1>
 
+              <p className="text-gray-700">{job.employer_company_type}</p>
+              <p className="text-gray-700">{job.employer_name}</p>
+
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Apply Now
+              </button>
+            </div>
+          ))}
+      </div>
     </MainLayout>
   );
 };
